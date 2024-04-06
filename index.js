f2c3530c648cec939962b361c4e90e5d2929641e
Anish <gundaanish6b@gmail.com>
2024-04-06 09:06:18 +0530

    Initial structure of database re-designed
    Added connection to mysql server for db

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+require("dotenv").config();
 
-const errorController = require('./controllers/error');
-const sequelize = require('./util/database');
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const errorController = require("./controllers/error");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const studentRoutes = require("./Routes/student");
+// const adminRoutes = require("./Routes/admin");
+// const companyRoutes = require("./Routes/company");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
-
+app.use("/s", studentRoutes);
+// app.use("/a", adminRoutes);
+// app.use("/c", companyRoutes);
 app.use(errorController.get404);
+
+app.listen( process.env.PORT || 3000, () => {
+  console.log("server listening on port: 3000");
+});
