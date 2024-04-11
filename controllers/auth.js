3c4294ee5601e787c7936b1a44d9cabd5034ca0c
duvvurisrinath <duvvurisrinath@gmail.com>
2024-04-11 15:30:23 +0530

    added authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,65 @@
 const pool = require("../util/connectionPool");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.postLogin = (req, res, next) => {
-  return res.redirect("/s");
+  console.log(req.headers);
+  const user_id = req.body.username;
+  const user_pass = req.body.password;
+  const user_role = req.body.role;
+
+  let tok = "";
+  let valid = false;
+  if (
+    req.headers.authorization != "" &&
+    req.headers.authorization != undefined
+  ) {
+    tok = req.headers.authorization.split(" ")[1];
+    jwt
+      .verify(tok, process.env.TOKEN_SECRET)
+      .then((result) => {
+        console.log("Token verified: \n", result);
+      })
+      .catch((err) => console.log(err));
+  }
+  
+  pool
+    .execute("SELECT * FROM USERS WHERE USER_ID = ? AND USER_ROLE = ?", [
+      user_id,
+      user_role,
+    ])
+    .then(([rows, fields]) => {
+      if (!rows.length) {
+        return console.log("User Not Found!!");
+      }
+
+      bcrypt
+        .compare(user_pass, rows[0].USER_PASS)
+        .then((result) => {
+          console.log(result);
+          if (result) {
+            let token = "invalid";
+            token = jwt.sign(
+              { user_id: user_id, user_role: user_role },
+              process.env.TOKEN_SECRET,
+              {
+                algorithm: "HS256",
+                expiresIn: 2 * 60 * 60,
+              }
+            );
+            res.status(200).send({ token: token });
+          } else {
+            return res.status(401).send("Invalid Password");
+          }
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
+  // res.json({ token: "authenticated" });
 };
 
 exports.is_auth = (req, res, next) => {
+  console.log("Here\n");
   valid = true;
   if (valid) return next();
   console.log("failed");
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const cors = require("cors");
 
 const authController = require("./controllers/auth");
 const errorController = require("./controllers/error");
@@ -17,15 +18,16 @@ app.set("views", "views");
 const studentRoutes = require("./Routes/student");
 const companyRoutes = require("./Routes/company");
 // const adminRoutes = require("./Routes/admin");
-
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 // app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/update", authController.updatePass);
 app.post("/login", authController.postLogin);
 app.use(authController.is_auth);
 app.use("/s", authController.is_student, studentRoutes);
-app.use("/c", authController.is_company,  companyRoutes);
+app.use("/c", authController.is_company, companyRoutes);
 // app.use("/a", adminRoutes);
 app.use(errorController.get404);
 
